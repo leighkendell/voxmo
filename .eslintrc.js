873c00bf6d47b5def02cd78cd83d1199d6bb9df2
Leigh Kendell <leighkendell@gmail.com>
2019-04-07 22:27:57 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  plugins: [
+    "@typescript-eslint",
+    "prettier"
+  ],
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 100
+      }
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+  },
+}
